e51bace7ffdc31d6d2645d96e7240a075fe797b5
Aoang <aoang@x2oe.com>
2022-08-18 22:54:07 +0800

    add tests

--- pool/pool_test.go
+++ pool/pool_test.go
@@ -1,6 +1,7 @@
 package pool
 
 import (
+	"sync"
 	"testing"
 )
 
@@ -17,4 +18,22 @@ func TestPool(t *testing.T) {
 	p := NewPool[string](nil, nil)
 	a := p.Get()
 	p.Put(a)
+}
+
+func TestPoolStore(t *testing.T) {
+	Put[int](nil)
+	if _, ok := pools.Load(0); ok {
+		t.Error("fail")
+	}
+
+	defer func() {
+		if err := recover(); err == nil {
+			t.Error("fail")
+		}
+	}()
+
+	p := &sync.Pool{New: func() interface{} { return new(string) }}
+	p.Put(0)
+	pools.Store(0, p)
+	Get[int]()
 }
