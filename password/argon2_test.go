a4e92176bc650b223e0eee177ead8c1738fc3a74
Aoang <aoang@x2oe.com>
2022-08-19 11:51:28 +0800

    Add tests

--- password/argon2_test.go
+++ password/argon2_test.go
@@ -1,6 +1,7 @@
 package password
 
 import (
+	"encoding/base64"
 	"testing"
 )
 
@@ -14,4 +15,19 @@ func testCompare(t *testing.T, g Generator, password string) {
 
 func TestArgon2(t *testing.T) {
 	testCompare(t, New(), "password")
+}
+
+func TestArgon2_CompareFail(t *testing.T) {
+	g := New()
+	t.Run("", func(t *testing.T) {
+		if g.Compare("", "") {
+			t.Error("fail")
+		}
+	})
+
+	t.Run("", func(t *testing.T) {
+		if g.Compare(base64.RawStdEncoding.EncodeToString([]byte("1234567890")), "") {
+			t.Error("fail")
+		}
+	})
 }
