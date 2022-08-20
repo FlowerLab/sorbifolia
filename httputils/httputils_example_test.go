d79f773c149fbfa7820924025c34cadd080ee59c
Aoang <aoang@x2oe.com>
2022-08-20 18:41:02 +0800

    Add tests

--- httputils/httputils_example_test.go
+++ httputils/httputils_example_test.go
@@ -3,6 +3,8 @@ package httputils
 import (
 	"testing"
 	"time"
+
+	"github.com/valyala/fasthttp"
 )
 
 type X2oxIPResp struct {
@@ -24,4 +26,36 @@ func TestHTTP(t *testing.T) {
 		t.Error(err)
 	}
 	t.Log(resp.IP)
+}
+
+func TestHTTPRetry(t *testing.T) {
+	var resp X2oxIPResp
+	err := Get("https://api.ip.x2ox.com/api/ip?type=json").
+		SetContentType(AppJSON).
+		Request(-1, nil, 5*time.Second).
+		ParserData(JSONParser(&resp)).
+		DoRelease()
+
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(resp.IP)
+}
+
+func TestHTTPError(t *testing.T) {
+	var resp X2oxIPResp
+	err := Post("https://api.ip.x2ox.com/api/ip?type=json").
+		SetContentType(AppJSON).
+		SetBodyWithEncoder(JSON(), struct {
+			A chan struct{}
+		}{A: nil}).
+		Request(3, func(err error, response *fasthttp.Response) bool {
+			return true
+		}, 5*time.Second).
+		ParserData(JSONParser(&resp)).
+		DoRelease()
+
+	if err == nil {
+		t.Error("err")
+	}
 }
