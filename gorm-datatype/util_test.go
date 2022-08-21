b8c5216665ec6dd4ff77450086d5eefc44466473
Aoang <aoang@x2oe.com>
2022-08-21 15:22:26 +0800

    Add test

--- gorm-datatype/util_test.go
+++ gorm-datatype/util_test.go
@@ -6,6 +6,28 @@ import (
 
 func TestScanLinearArray(t *testing.T) {
 	for _, tt := range []struct {
+		input string
+		dims  []int
+		elems [][]byte
+	}{
+		{`{}`, nil, [][]byte{}},
+		{`{NULL}`, []int{1}, [][]byte{nil}},
+		{`{a}`, []int{1}, [][]byte{{'a'}}},
+		{`{""}`, []int{1}, [][]byte{{}}},
+		{`{","}`, []int{1}, [][]byte{{','}}},
+		{`{",",","}`, []int{2}, [][]byte{{','}, {','}}},
+		{`{"\"}"}`, []int{1}, [][]byte{{'"', '}'}}},
+		{`{"\"","\""}`, []int{2}, [][]byte{{'"'}, {'"'}}},
+	} {
+		_, err := scanLinearArray([]byte(tt.input), "")
+		if err != nil {
+			t.Fatalf("Expected no error for %q, got %q", tt.input, err)
+		}
+	}
+}
+
+func TestScanLinearArrayErr(t *testing.T) {
+	for _, tt := range []struct {
 		input, err string
 	}{
 		{``, "expected '{' at offset 0"},
@@ -23,6 +45,7 @@ func TestScanLinearArray(t *testing.T) {
 		{`{{x}`, "expected '}' at offset 4"},
 		{`{""x}`, "unexpected 'x' at offset 3"},
 		{`{{a},{b,c}}`, "multidimensional arrays must have elements with matching dimensions"},
+		{`{{",",","}}`, "cannot convert ARRAY"},
 	} {
 		_, err := scanLinearArray([]byte(tt.input), "")
 		if err == nil {
