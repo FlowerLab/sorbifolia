dc039b605e1f048e3c1cbb16e6ebae03b7aea767
Aoang <aoang@x2oe.com>
2022-08-21 20:33:34 +0800

    Add test

--- gorm-datatype/array_string_test.go
+++ gorm-datatype/array_string_test.go
@@ -47,6 +47,23 @@ func TestArrayString_Scan(t *testing.T) {
 		}
 	}
 
+	for _, tt := range []struct {
+		str string
+		arr []string
+	}{
+		{`{}`, nil},
+		{`{t}`, []string{"t"}},
+		{`{f,1}`, []string{"f", "1"}},
+		{`{"a\\b","c d",","}`, []string{"a\\b", "c d", ","}},
+	} {
+		a := ArrayString{}
+		err := a.Scan(tt.str)
+
+		if err != nil {
+			t.Errorf("expected no error for %s, got %v", tt.str, err)
+		}
+	}
+
 	a := ArrayString{}
 	if err := a.Scan(nil); err != nil {
 		t.Errorf("expected no error for nil, got %v", err)
@@ -87,6 +104,11 @@ func TestArrayString_Value(t *testing.T) {
 
 	a = []string{}
 	if val, err := a.Value(); err != nil || val != "{}" {
+		t.Error("expected")
+	}
+
+	a = []string{"a", "b", "c", "d", "e", "f"}
+	if val, err := a.Value(); err != nil || val != "{\"a\",\"b\",\"c\",\"d\",\"e\",\"f\"}" {
 		t.Error("expected")
 	}
 }
