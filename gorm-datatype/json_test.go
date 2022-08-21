7057d00478839efd50892c6c0d132a645e447e4e
Aoang <aoang@x2oe.com>
2022-08-21 15:50:15 +0800

    Add test

--- gorm-datatype/json_test.go
+++ gorm-datatype/json_test.go
@@ -1,11 +1,14 @@
 package datatype
 
 import (
+	"bytes"
 	"encoding/json"
 	"testing"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
+	"gorm.io/gorm/schema"
 )
 
 var db *gorm.DB
@@ -42,4 +45,99 @@ func TestJSON(t *testing.T) {
 	if err := db.Create(t1).Error; err != nil {
 		t.Error(err)
 	}
+}
+
+func TestJSON_Value(t *testing.T) {
+	var i JSON
+	if val, err := i.Value(); err != nil || val != nil {
+		t.Error("fail")
+	}
+
+	i = []byte(`{"a":"a"}`)
+	if val, _ := i.Value(); val != `{"a":"a"}` {
+		t.Error("fail")
+	}
+}
+
+func TestJSON_Scan(t *testing.T) {
+	var i JSON
+
+	if err := i.Scan(nil); err != nil {
+		t.Error(err)
+	}
+	if err := i.Scan([]byte(`{"a":"a"}`)); err != nil {
+		t.Error(err)
+	}
+	if err := i.Scan(`{"a":"a"}`); err != nil {
+		t.Error(err)
+	}
+	if err := i.Scan(map[string]string{"a": "a"}); err == nil {
+		t.Error(err)
+	}
+}
+
+func TestJSON_MarshalJSON(t *testing.T) {
+	var i JSON
+	if err := i.UnmarshalJSON([]byte(`{"a":"a"}`)); err != nil {
+		t.Error(err)
+	}
+	if bts, err := i.MarshalJSON(); err != nil || !bytes.Equal(bts, []byte("{\"a\":\"a\"}")) {
+		t.Error(err)
+	}
+}
+
+func TestJSON_GormDataType(t *testing.T) {
+	var i JSON
+	if i.GormDataType() != "json" {
+		t.Error("GormDataType json")
+	}
+}
+
+func TestJSON_GormDBDataType(t *testing.T) {
+	var (
+		i      JSON
+		testDB = &gorm.DB{Config: &gorm.Config{Dialector: testDialector("postgres")}}
+	)
+
+	if i.GormDBDataType(testDB, nil) != "JSONB" {
+		t.Error("fail")
+	}
+	testDB = &gorm.DB{Config: &gorm.Config{Dialector: testDialector("mysql")}}
+	if i.GormDBDataType(testDB, nil) != "" {
+		t.Error("fail")
+	}
+}
+
+type testDialector string
+
+func (t testDialector) Name() string {
+	return string(t)
+}
+
+func (t testDialector) Initialize(d *gorm.DB) error {
+	panic("implement me")
+}
+
+func (t testDialector) Migrator(db *gorm.DB) gorm.Migrator {
+	panic("implement me")
+}
+
+func (t testDialector) DataTypeOf(field *schema.Field) string {
+	panic("implement me")
+}
+
+func (t testDialector) DefaultValueOf(field *schema.Field) clause.Expression {
+	panic("implement me")
+}
+
+func (t testDialector) BindVarTo(writer clause.Writer, stmt *gorm.Statement, v interface{}) {
+	panic("implement me")
+}
+
+func (t testDialector) QuoteTo(writer clause.Writer, s string) {
+	panic("implement me")
+}
+
+func (t testDialector) Explain(sql string, vars ...interface{}) string {
+	panic("implement me")
 }
