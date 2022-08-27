b53fe825afe23cee17f4a9b3748bd301300d4989
Aoang <aoang@x2oe.com>
2022-08-27 15:43:04 +0800

    Add NewMethod

--- httprouter/method.go
+++ httprouter/method.go
@@ -1,5 +1,9 @@
 package httprouter
 
+import (
+	"strings"
+)
+
 type MethodNode[T any] struct {
 	Method Method
 	Node   *Node[T]
@@ -20,3 +24,28 @@ const (
 )
 
 var methods = []Method{GET, HEAD, POST, PUT, PATCH, DELETE, CONNECT, OPTIONS, TRACE}
+
+func NewMethod(method string) Method {
+	switch strings.ToUpper(method) {
+	case "GET":
+		return GET
+	case "HEAD":
+		return HEAD
+	case "POST":
+		return POST
+	case "PUT":
+		return PUT
+	case "PATCH":
+		return PATCH
+	case "DELETE":
+		return DELETE
+	case "CONNECT":
+		return CONNECT
+	case "OPTIONS":
+		return OPTIONS
+	case "TRACE":
+		return TRACE
+	}
+
+	return 255
+}
