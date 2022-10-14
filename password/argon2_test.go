ed48fe6c2a6b14afa5daf71f37a567a3bdcbe9c0
Aoang <aoang@x2oe.com>
2022-10-14 16:49:01 +0800

    Fix race tests

--- password/argon2_test.go
+++ password/argon2_test.go
@@ -1,12 +1,9 @@
 package password
 
 import (
-	"crypto/rand"
 	_ "crypto/rand"
 	"encoding/base64"
-	"errors"
 	"testing"
-	"time"
 )
 
 func testCompare(t *testing.T, g Generator, password string) {
@@ -42,25 +39,4 @@ func TestArgon2_CompareFail(t *testing.T) {
 			t.Error("fail")
 		}
 	})
-}
-
-type errReader struct{}
-
-func (e errReader) Read([]byte) (n int, err error) {
-	return 0, errors.New("OEF")
-}
-
-func TestArgon2_MustGenerate(t *testing.T) {
-	t.Parallel()
-
-	time.Sleep(time.Second) // It changes rand.Reader
-	g := New()
-
-	rand.Reader = errReader{}
-
-	defer func() { _ = recover() }()
-
-	g.MustGenerate("123456")
-
-	t.Error("fail")
 }
