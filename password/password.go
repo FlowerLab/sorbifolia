4c85880c37b49533dcda75d7042708726efd7d2f
Aoang <aoang@x2oe.com>
2022-10-14 17:22:31 +0800

    Add Password Function (#41)
    * Add Password Function
    
    * Fix race tests
    
    * Fix race tests
    
    * Update tests

--- password/password.go
+++ password/password.go
@@ -8,3 +8,12 @@ type Generator interface {
 	Generate(password string) (string, error)
 	Compare(hashedPassword, password string) bool
 }
+
+// defaultGenerator may change the underlying algorithm in the future
+var defaultGenerator = New()
+
+func MustGenerate(password string) string      { return defaultGenerator.MustGenerate(password) }
+func Generate(password string) (string, error) { return defaultGenerator.Generate(password) }
+func Compare(hashedPassword, password string) bool {
+	return defaultGenerator.Compare(hashedPassword, password)
+}
