205a0d7b687d76c98b8dbf8e8a9d73a1b190d7bd
Yaqf <yeovng@gmail.com>
2022-10-17 00:12:59 +0800

    Add test case

--- strutils/utils_test.go
+++ strutils/utils_test.go
@@ -9,10 +9,12 @@ var checkUTF8LenTests = []struct {
 	b    []byte
 	size int
 }{
-	{[]byte{'a'}, 1},
-	{[]byte("é"), 2},
-	{[]byte("我"), 3},
-	{[]byte(string('\U0010FFFF')), 4},
+	{[]byte{'a'}, 1},                  // as
+	{[]byte("é"), 2},                  // s1
+	{[]byte("我"), 3},                  // s3
+	{[]byte(string('\U0010FFFF')), 4}, // s7
+
+	{[]byte{0xe0, 0b10110001, 0b10100001}, 3}, // s2
 }
 
 func Test_checkUTF8Len(t *testing.T) {
