83ae8e5fd6f2f6f13e01d9a76ca6c8a1951583e7
Yaqf <q@x2oe.com>
2022-10-17 15:00:41 +0800

    Add tests (#45)
    * Add test case
    
    * Add last test case
    
    * Add others cases test
    
    https://github.com/FlowerLab/sorbifolia/pull/45#issuecomment-1280277121

--- strutils/utils_test.go
+++ strutils/utils_test.go
@@ -9,10 +9,30 @@ var checkUTF8LenTests = []struct {
 	b    []byte
 	size int
 }{
-	{[]byte{'a'}, 1},
-	{[]byte("é"), 2},
-	{[]byte("我"), 3},
-	{[]byte(string('\U0010FFFF')), 4},
+	{[]byte{'a'}, 1},                  // as
+	{[]byte("é"), 2},                  // s1
+	{[]byte("我"), 3},                  // s3
+	{[]byte(string('\U0010FFFF')), 4}, // s7
+	{[]byte{0xf4, 0b10000100, 0b10000101, 0b10000110}, 4}, // s7
+
+	{[]byte{0xe0, 0b10110001, 0b10100001}, 3},             // s2
+	{[]byte{0xed, 0b10011111, 0b10000111}, 3},             // s4
+	{[]byte{0xf0, 0b10010000, 0b10011000, 0b10011100}, 4}, // s5
+	{[]byte{0xf1, 0b10000010, 0b10100000, 0b10101010}, 4}, // s6
+
+	{[]byte{0xa9}, 4}, // xx
+
+	{[]byte{}, 1}, // line 6
+
+	// add test for p[1] < accept.lo || accept.hi < p[1]
+	{[]byte{0xc2, 0b00110001}, 2}, // line 22
+	{[]byte{0xc2, 0b11111111}, 2}, // line 22
+
+	{[]byte{0xe0, 0b10110001, 0b00110001}, 3}, // line 29
+	{[]byte{0xe0, 0b10110001, 0b11111111}, 3}, // line 29
+
+	{[]byte{0xf1, 0b10110001, 0b10011000, 0b00110001}, 4}, // line 36
+	{[]byte{0xf1, 0b10110001, 0b10011000, 0b11111111}, 4}, // line 36
 }
 
 func Test_checkUTF8Len(t *testing.T) {
