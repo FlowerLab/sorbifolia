2ee65708d38265c9eb4afe8891f16d98a0cc0149
yaqf <yeovng@gmail.com>
2022-10-17 12:08:51 +0800

    Add last test case

--- strutils/utils_test.go
+++ strutils/utils_test.go
@@ -13,8 +13,14 @@ var checkUTF8LenTests = []struct {
 	{[]byte("é"), 2},                  // s1
 	{[]byte("我"), 3},                  // s3
 	{[]byte(string('\U0010FFFF')), 4}, // s7
+	{[]byte{0xf4, 0b10000100, 0b10000101, 0b10000110}, 4}, // s7
 
-	{[]byte{0xe0, 0b10110001, 0b10100001}, 3}, // s2
+	{[]byte{0xe0, 0b10110001, 0b10100001}, 3},             // s2
+	{[]byte{0xed, 0b10011111, 0b10000111}, 3},             // s4
+	{[]byte{0xf0, 0b10010000, 0b10011000, 0b10011100}, 4}, // s5
+	{[]byte{0xf1, 0b10000010, 0b10100000, 0b10101010}, 4}, // s6
+
+	{[]byte{0xa9}, 4}, // xx
 }
 
 func Test_checkUTF8Len(t *testing.T) {
