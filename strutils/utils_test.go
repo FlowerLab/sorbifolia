b6a307a7f86d8ec6fb1d6a356fda5e1e08b10daf
yaqf <yeovng@gmail.com>
2022-10-17 14:34:44 +0800

    Add others cases test
    https://github.com/FlowerLab/sorbifolia/pull/45#issuecomment-1280277121

--- strutils/utils_test.go
+++ strutils/utils_test.go
@@ -21,6 +21,18 @@ var checkUTF8LenTests = []struct {
 	{[]byte{0xf1, 0b10000010, 0b10100000, 0b10101010}, 4}, // s6
 
 	{[]byte{0xa9}, 4}, // xx
+
+	{[]byte{}, 1}, // line 6
+
+	// add test for p[1] < accept.lo || accept.hi < p[1]
+	{[]byte{0xc2, 0b00110001}, 2}, // line 22
+	{[]byte{0xc2, 0b11111111}, 2}, // line 22
+
+	{[]byte{0xe0, 0b10110001, 0b00110001}, 3}, // line 29
+	{[]byte{0xe0, 0b10110001, 0b11111111}, 3}, // line 29
+
+	{[]byte{0xf1, 0b10110001, 0b10011000, 0b00110001}, 4}, // line 36
+	{[]byte{0xf1, 0b10110001, 0b10011000, 0b11111111}, 4}, // line 36
 }
 
 func Test_checkUTF8Len(t *testing.T) {
