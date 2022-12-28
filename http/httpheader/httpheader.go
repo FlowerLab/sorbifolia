9ed8affe938d58641fbdfaa053494cfadac2ee3d
Aoang <aoang@x2oe.com>
2022-12-28 16:14:57 +0800

    http: add http header

--- http/httpheader/httpheader.go
+++ http/httpheader/httpheader.go
@@ -1,21 +1,14 @@
 package httpheader
 
-import (
-	"go.x2ox.com/sorbifolia/http/internal/char"
+type QualityValue struct {
+	Value    []byte
+	Priority float64 // 1.00 - 0.00
+}
+
+type (
+	EachKeyQualityValue func(key []byte, val QualityValue) bool
+	EachQualityValue    func(val QualityValue) bool
+	EachValue           func(val []byte) bool
+	EachKeyValue        func(key, val []byte) bool
+	EachRanger          func(r Ranger) bool
 )
-
-func cleanTrailingSpaces(b []byte) []byte {
-	for i := len(b) - 1; i >= 0; i-- {
-		if b[i] != char.Space {
-			return b[:i]
-		}
-	}
-	return nil
-}
-
-func cleanQuotationMark(b []byte) []byte {
-	if len(b) < 2 || b[0] != char.QuotationMark || b[len(b)-1] != char.QuotationMark {
-		return b
-	}
-	return b[1 : len(b)-1]
-}
