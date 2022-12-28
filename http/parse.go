82c4647007f01bb88e9ffa9a79e390e629f22e27
Aoang <aoang@x2oe.com>
2022-12-28 18:32:31 +0800

    http: add httperr

--- http/parse.go
+++ http/parse.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"net"
 
+	"go.x2ox.com/sorbifolia/http/httperr"
 	"go.x2ox.com/sorbifolia/http/internal/bodyio"
 	"go.x2ox.com/sorbifolia/http/internal/char"
 	"go.x2ox.com/sorbifolia/http/internal/util"
@@ -15,32 +16,36 @@ import (
 	"go.x2ox.com/sorbifolia/http/version"
 )
 
-func (s *Server) ParseRequestHeader(conn net.Conn, a *arena.Arena) (*Request, error) {
+func (s *Server) ParseRequestHeader(conn net.Conn, a *arena.Arena) (req *Request, err error) {
+	req = arena.New[Request](a)
+
 	buf := arena.MakeSlice[byte](a, s.MaxRequestHeaderSize, s.MaxRequestHeaderSize)
-	n, err := conn.Read(buf)
-	if err != nil {
-		return nil, err
+
+	var n int
+	if n, err = conn.Read(buf); err != nil {
+		return
 	}
 	buf = buf[:n]
 
 	idx := bytes.Index(buf, char.CRLF) // first line
 	if idx == -1 {
-		return nil, fmt.Errorf("parsing protocol header error")
+		err = fmt.Errorf("parsing protocol header error")
+		return
 	}
 	arr := util.Split(a, buf[:idx], char.Spaces)
 	if len(arr) != 3 {
-		return nil, fmt.Errorf("parsing protocol header error")
+		err = fmt.Errorf("parsing protocol header error")
+		return
 	}
 
-	req := arena.New[Request](a)
-	// req.Header.URL=
 	req.Method = method.Parse(util.ToUpper(arr[0]))
 	req.Header.RequestURI = arr[1]
 	req.ver, _ = version.Parse(arr[2])
 
 	ei := bytes.Index(buf, char.CRLF2) // end position index
 	if ei == -1 {
-		return nil, fmt.Errorf("413 Entity Too Large")
+		err = httperr.RequestHeaderFieldsTooLarge
+		return
 	}
 
 	arr = util.Split(a, buf[idx+2:ei], char.CRLF) // header
@@ -51,14 +56,17 @@ func (s *Server) ParseRequestHeader(conn net.Conn, a *arena.Arena) (*Request, er
 	req.Header.KVs = (*KVs)(&kvs)
 
 	if err = req.Header.init(); err != nil {
-		return nil, err
+		return
 	}
+	// if len(req.Header.ContentLength) == 0 && req.Method {
+	//
+	// }
 
 	// TODO length, check buf[ei+4:n] and conn
 	if length := req.Header.ContentLength.Length(); length == 0 {
 		req.Body = bodyio.Null()
 	} else if length > s.MaxRequestBodySize {
-		return nil, err // body is too large
+		return nil, httperr.BodyTooLarge // body is too large
 	} else if length > s.StreamRequestBodySize {
 		req.Body, err = bodyio.File(a, buf[ei+4:], conn, length)
 	} else if s.StreamRequestBodySize < 0 {
@@ -67,5 +75,5 @@ func (s *Server) ParseRequestHeader(conn net.Conn, a *arena.Arena) (*Request, er
 		req.Body, err = bodyio.Memory(a, buf[ei+4:], conn, length)
 	}
 
-	return req, err
+	return
 }
