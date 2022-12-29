942ecc6c4c2c2bd5c5a1ddc74644c426c32be6db
Aoang <aoang@x2oe.com>
2022-12-29 19:31:38 +0800

    http: add chunked

--- http/internal/util/util.go
+++ http/internal/util/util.go
@@ -1,1 +1,11 @@
 package util
+
+func ToNonNegativeInt64(b []byte) (n int64) {
+	for _, val := range b {
+		if val > '9' || val < '0' {
+			return -1
+		}
+		n = n*10 + int64(val-'0')
+	}
+	return
+}
