8275b8954207f6a05ccf974f3ab7445c7a7c2956
Aoang <aoang@x2oe.com>
2023-01-06 15:47:48 +0800

    http: add nobody

---
+++ http/httpbody/nobody.go
@@ -0,0 +1,15 @@
+package httpbody
+
+import (
+	"io"
+)
+
+var _nobody = nobody{}
+
+type nobody struct{}
+
+func (nobody) Write(_ []byte) (int, error) { return 0, io.EOF }
+func (nobody) Read(_ []byte) (int, error)  { return 0, io.EOF }
+func (nobody) Close() error                { return nil }
+
+func Null() io.ReadWriteCloser { return _nobody }
