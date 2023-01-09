fe05ccdc9777b49f995482d94851266212b7695f
lixvda <1547446385@qq.com>
2023-01-09 11:16:58 +0800

    fix: method name

--- http/version/parse_test.go
+++ http/version/parse_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestParseHttpVersion(t *testing.T) {
+func TestParse(t *testing.T) {
 	major, minor, ok := parseHTTPVersion([]byte("HTTP/2.0"))
 	if !ok {
 		t.Error("parse error")
