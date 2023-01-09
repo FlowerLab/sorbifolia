558c72f5d8f9cb37aff221d4f7ae072846ed436c
Pinkpig <59948049+Ignorant-lxf@users.noreply.github.com>
2023-01-09 19:05:55 +0800

    fix: parse version (#72)

---
+++ http/version/parse_test.go
@@ -0,0 +1,30 @@
+package version
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	testVersion := []Version{
+		{Major: 0, Minor: 9},
+		{Major: 1, Minor: 0},
+		{Major: 1, Minor: 1},
+		{Major: 2, Minor: 0},
+		{Major: 3, Minor: 0},
+	}
+
+	for i, v := range testVersion {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			version, ok := Parse(v.Bytes())
+			if !ok {
+				t.Error("parse error")
+			}
+
+			if !reflect.DeepEqual(v, version) {
+				t.Errorf("expected %v,got %v", v, version)
+			}
+		})
+	}
+}
