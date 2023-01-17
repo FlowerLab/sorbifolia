bfece84f5277e707cef97ff33d34020e9150a89d
Pinkpig <59948049+Ignorant-lxf@users.noreply.github.com>
2023-01-17 10:56:47 +0800

    http: add chunk test (#79)
    * http: chunked write test
    
    * http: fix data race
    
    * http: test race
    
    * http: test race
    
    * http: rm mtx
    
    * http: fix lint
    
    * http: rm wg
    
    * http: fix data race
    
    * http: fix coverage
    
    * http: fix data race
    
    * http: code format
    
    * http: code format
    
    * http: code format
    
    * http: code format
    
    * http: fix struct

--- http/httpbody/chunked_test.go
+++ http/httpbody/chunked_test.go
@@ -7,6 +7,8 @@ import (
 	"reflect"
 	"sync"
 	"testing"
+
+	"go.x2ox.com/sorbifolia/http/internal/bufpool"
 )
 
 func TestRead(t *testing.T) {
@@ -50,4 +52,130 @@ func TestRead(t *testing.T) {
 
 	_ = chunked.Close()
 	chunked.release()
+}
+
+func TestWrite(t *testing.T) {
+	tests := []TC{
+		{
+			Chunked: &Chunked{
+				Data:   make(chan []byte),
+				Header: make(chan []byte),
+				m:      ModeReadWrite,
+				finish: false,
+				state:  0,
+				once:   sync.Once{},
+				buf:    bufpool.Buffer{},
+			},
+			Data: []byte("7\r\nhello, \r\n" +
+				"6\r\nworld!\r\n" +
+				"0\r\n" +
+				"Expires: Fri, 20 Jan 2023 07:28:00 GMT\r\n" +
+				"\r\n"),
+			Res: "",
+			Fn: func(chunked *Chunked) {
+				close(chunked.Data)
+				close(chunked.Header)
+			},
+			Err: io.EOF,
+		},
+		{
+			Chunked: &Chunked{
+				Data:   make(chan []byte),
+				Header: make(chan []byte),
+				m:      ModeWrite,
+				finish: false,
+				state:  chunkedEND,
+				once:   sync.Once{},
+				buf:    bufpool.Buffer{},
+			},
+			Data: []byte("7\r\nhello, \r\n" +
+				"6\r\nworld!\r\n" +
+				"0\r\n" +
+				"Expires: Fri, 20 Jan 2023 07:28:00 GMT\r\n" +
+				"\r\n"),
+			Res: "",
+			Fn: func(chunked *Chunked) {
+				close(chunked.Data)
+				close(chunked.Header)
+			},
+			Err: io.EOF,
+		},
+		{
+			Chunked: &Chunked{
+				Data:   make(chan []byte),
+				Header: make(chan []byte),
+				m:      ModeWrite,
+				finish: false,
+				once:   sync.Once{},
+				buf:    bufpool.Buffer{},
+			},
+			Data: []byte("7\r\nhello, \r\n" +
+				"6\r\nworld!\r\n" +
+				"0\r\n" +
+				"Expires: Fri, 20 Jan 2023 07:28:00 GMT\r\n" +
+				"\r\n"),
+			Res: "Expires: Fri, 20 Jan 2023 07:28:00 GMT",
+			Fn:  nil,
+			Err: io.EOF,
+		},
+		{
+
+			Chunked: &Chunked{
+				Data:   make(chan []byte),
+				Header: make(chan []byte),
+				m:      ModeWrite,
+				finish: false,
+				once:   sync.Once{},
+				buf:    bufpool.Buffer{B: []byte("\r")},
+			},
+			Data: []byte("\nhello, "),
+			Res:  "",
+			Fn: func(chunked *Chunked) {
+				close(chunked.Data)
+				close(chunked.Header)
+			},
+			Err: nil,
+		},
+	}
+
+	var wg sync.WaitGroup
+	for i, v := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			wg.Add(1)
+			go func() {
+				_, err := io.Copy(v.Chunked, bytes.NewReader(v.Data))
+				if !reflect.DeepEqual(err, v.Err) {
+					t.Errorf("expected: %v,got: %v", v.Err, err)
+				}
+				if v.Fn != nil {
+					v.Fn(v.Chunked)
+				}
+				wg.Done()
+			}()
+
+			var (
+				ok  = true
+				buf = new(bytes.Buffer)
+			)
+			for ok {
+				_, ok = <-v.Chunked.Data
+			}
+			for hv := range v.Header {
+				buf.Write(hv)
+			}
+
+			if !reflect.DeepEqual(v.Res, buf.String()) {
+				t.Errorf("expected: %v,got: %v", v.Res, buf.String())
+			}
+			wg.Wait()
+		})
+	}
+}
+
+type TC struct {
+	*Chunked
+	Data []byte
+	Res  string
+	Fn   func(c *Chunked)
+	Err  error
 }
