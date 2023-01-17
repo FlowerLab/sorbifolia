3aab1f12019a9aaa39513d0b0cc495814c271635
lixvda <1547446385@qq.com>
2023-01-17 15:31:03 +0800

    http: add memory write test

--- http/httpbody/memory_test.go
+++ http/httpbody/memory_test.go
@@ -25,16 +25,12 @@ func TestMemoryRead(t *testing.T) {
 		t.Errorf("expected: %v got: %v", string(data), buf.String())
 	}
 
+	_ = memo.Close()
 	memo.release()
 }
 
-type MER struct {
-	*Memory
-	Err error
-}
-
 func TestMemoryErrorRead(t *testing.T) {
-	test := []MER{
+	tests := []MER{
 		{
 			Memory: &Memory{
 				buf:  bufpool.Buffer{},
@@ -61,13 +57,83 @@ func TestMemoryErrorRead(t *testing.T) {
 		},
 	}
 
-	for i, v := range test {
+	for i, v := range tests {
 		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
 			buf := new(bytes.Buffer)
 			_, err := io.Copy(buf, v.Memory)
 			if !reflect.DeepEqual(v.Err, err) {
 				t.Errorf("expected: %v,got: %v", v.Err, err)
 			}
+			_ = v.Memory.Close()
 		})
 	}
+}
+
+func TestMemoryWrite(t *testing.T) {
+	tests := []MW{
+		{
+			Memory: &Memory{
+				buf:  bufpool.Buffer{},
+				p:    0,
+				mode: ModeReadWrite,
+			},
+			Data: []byte("11\r\nhello,world\r\n"),
+			Res:  "11\r\nhello,world\r\n",
+			Err:  nil,
+		},
+		{
+			Memory: &Memory{
+				buf:  bufpool.Buffer{},
+				p:    0,
+				mode: ModeWrite,
+			},
+			Data: []byte("123456"),
+			Res:  "123456",
+			Err:  nil,
+		},
+		{
+			Memory: &Memory{
+				buf:  bufpool.Buffer{},
+				p:    0,
+				mode: ModeRead,
+			},
+			Data: []byte("123456"),
+			Res:  "",
+			Err:  io.EOF,
+		},
+		{
+			Memory: &Memory{
+				buf:  bufpool.Buffer{},
+				p:    0,
+				mode: ModeClose,
+			},
+			Data: []byte("123456"),
+			Res:  "",
+			Err:  io.EOF,
+		},
+	}
+
+	for i, v := range tests {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			_, err := io.Copy(v.Memory, bytes.NewReader(v.Data))
+			if !reflect.DeepEqual(v.Err, err) {
+				t.Errorf("expected: %v,got: %v", v.Err, err)
+			}
+			if !reflect.DeepEqual(v.Res, v.buf.String()) {
+				t.Errorf("expected: %v,got: %v", v.Res, v.buf.String())
+			}
+		})
+	}
+}
+
+type MER struct {
+	*Memory
+	Err error
+}
+
+type MW struct {
+	*Memory
+	Data []byte
+	Res  string
+	Err  error
 }
