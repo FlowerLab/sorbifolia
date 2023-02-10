a148dd587022581e24748a3dc54481b187a053d8
Pinkpig <59948049+Ignorant-lxf@users.noreply.github.com>
2023-02-10 14:49:36 +0800

    MFS Implement (#87)
    * memory-fs: finish implement
    
    * memory-fs: add dir test
    
    * memory-fs: dir_test fix name
    
    * memory-fs: add fs_test
    
    * memory-fs: perfect test
    
    * memory-fs: add mem-fs test
    
    * memory-fs: fix test
    
    * memory-fs: fix test
    
    memory-fs: fix test
    
    memory-fs: fix test
    
    memory-fs: fix test
    
    memory-fs: add test
    
    memory-fs: fix test
    
    memory-fs: fix test
    
    memory-fs: fix test
    
    memory-fs: fix test
    
    memory-fs: fix test
    
    memory-fs: test
    
    memory-fs: test
    
    memory-fs: test
    
    memory-fs: test
    
    memory-fs: test
    
    memory-fs: test
    
    memory-fs: test
    
    memory-fs: test
    
    memory-fs: test
    
    memory-fs: test
    
    memory-fs: test
    
    memory-fs: fix test
    
    memory-fs: fix test
    
    memory-fs: fix coverage
    
    * fix mfs
    
    * fix test
    
    * bfs to recursion
    
    * fix test
    
    * fix test
    
    * fix test
    
    * fix coverage
    
    * fix
    
    * fix coverage
    
    * fix ci
    
    * Update memory-fs/memory-fs.go
    
    Signed-off-by: Aoang <aoang@x2oe.com>
    
    * fix
    
    * fix ci
    
    * fix coverage
    
    * fix ci
    
    * fix
    
    ---------
    
    Signed-off-by: Aoang <aoang@x2oe.com>
    Co-authored-by: Aoang <aoang@x2oe.com>

--- memory-fs/memory-fs.go
+++ memory-fs/memory-fs.go
@@ -2,8 +2,10 @@ package mfs
 
 import (
 	"errors"
+	"fmt"
 	"io/fs"
 	"os"
+	"time"
 )
 
 type MemoryFS interface {
@@ -22,8 +24,121 @@ type openFS interface {
 	FS() fs.File
 }
 
-func New() MemoryFS                               { panic("not implemented") }
-func Fork(name string) (MemoryFS, error)          { panic("not implemented") }
-func Persistence(mfs MemoryFS, name string) error { panic("not implemented") }
+func New() MemoryFS {
+	return &mfs{
+		root: &dir{
+			name:    "/",
+			modTime: time.Now(),
+			node:    make(map[string]openFS),
+		},
+	}
+}
+
+func Fork(name string) (MemoryFS, error) {
+	return fork("/", name)
+}
+
+func fork(memDirname, name string) (MemoryFS, error) {
+	files, err := os.ReadDir(name)
+	if err != nil {
+		return nil, err
+	}
+
+	if name[len(name)-1] != '/' {
+		name += "/"
+	}
+
+	root := &dir{
+		name:    memDirname,
+		modTime: time.Now(),
+		node:    make(map[string]openFS),
+	}
+
+	for _, f := range files {
+		tf := fmt.Sprintf("%s%s", name, f.Name())
+
+		if !f.IsDir() {
+			var content []byte
+			if content, err = os.ReadFile(tf); err != nil {
+				return nil, err
+			}
+
+			root.node[f.Name()] = &file{
+				name:    f.Name(),
+				modTime: time.Now(),
+				data:    content,
+			}
+			continue
+		}
+
+		var cdir MemoryFS
+		if cdir, err = fork(f.Name(), tf); err != nil {
+			return nil, err
+		}
+		root.node[f.Name()] = cdir.(*mfs).root
+	}
+
+	return &mfs{root}, nil
+}
+
+func Persistence(m MemoryFS, name string) error {
+	f, err := os.Stat(name)
+	if err != nil {
+		if !os.IsExist(err) {
+			if err := os.MkdirAll(name, 0750); err != nil {
+				return err
+			}
+		}
+	}
+	if err == nil && !f.IsDir() {
+		return fmt.Errorf("%s is not a directory", name)
+	}
+
+	return persistence(m, name)
+}
+
+func persistence(m MemoryFS, diskName string) error {
+	d := m.(*mfs).root
+	if diskName[len(diskName)-1] != '/' {
+		diskName += "/"
+	}
+
+	for _, f := range d.node {
+		cpath := fmt.Sprintf("%s%s", diskName, f.Name())
+		if !f.IsDir() {
+			mf := f.(*file)
+			var cf *os.File
+			cf, err := os.Create(cpath)
+			if err != nil {
+				return err
+			}
+			if _, err = cf.Write(mf.data); err != nil {
+				return err
+			}
+			if err = cf.Close(); err != nil {
+				return err
+			}
+			continue
+		}
+
+		if !exists(cpath) {
+			if err := os.Mkdir(cpath, os.ModePerm); err != nil {
+				return err
+			}
+		}
+		if err := persistence(&mfs{f.(*dir)}, cpath); err != nil {
+			return err
+		}
+	}
+	return nil
+}
 
 var errIsDirectory = errors.New("is a directory")
+
+func exists(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
+		return os.IsExist(err)
+	}
+	return true
+}
