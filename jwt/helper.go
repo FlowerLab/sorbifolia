a4a415ad2c684c3cb60e6f04bdf9f97c97cc0786
Aoang <aoang@x2oe.com>
2023-10-27 13:04:40 +0800

    Upgrade golang-jwt dependency to v5
    The golang-jwt dependency has been updated from version 4 to version 5. This involved updates to import statements and adjustments to how signing methods are referenced in our jwt helper functions. Consequently, the go.sum and go.mod files were also updated to reflect this new dependency version.

--- jwt/helper.go
+++ jwt/helper.go
@@ -3,24 +3,7 @@ package jwt
 import (
 	"time"
 
-	"github.com/golang-jwt/jwt/v4"
-)
-
-var (
-	ES256 = jwt.SigningMethodES256
-	ES384 = jwt.SigningMethodES384
-	ES512 = jwt.SigningMethodES512
-	EdDSA = jwt.SigningMethodEdDSA
-	HS256 = jwt.SigningMethodHS256
-	HS384 = jwt.SigningMethodHS384
-	HS512 = jwt.SigningMethodHS512
-	RS256 = jwt.SigningMethodRS256
-	RS384 = jwt.SigningMethodRS384
-	RS512 = jwt.SigningMethodRS512
-	PS256 = jwt.SigningMethodPS256
-	PS384 = jwt.SigningMethodPS384
-	PS512 = jwt.SigningMethodPS512
-	None  = jwt.SigningMethodNone
+	"github.com/golang-jwt/jwt/v5"
 )
 
 func NewClaims[T any](data *T) *Claims[T]                 { return &Claims[T]{Data: data} }
