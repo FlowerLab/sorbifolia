e32f92432d7b407aca156c0a3d9c9c1d131ee910
Aoang <aoang@x2oe.com>
2023-12-06 16:51:50 +0800

    Add test cases for excluded IP ranges in cidr package
    Introduced new test cases in 'exclude_test.go' to expand the test coverage and added more complex scenarios involing partially containing IP ranges. The newly added tests for 'exclude' method help validate its accurate functionality within various circumstances in the cidr package.

--- cidr/exclude_test.go
+++ cidr/exclude_test.go
@@ -1,6 +1,9 @@
 package cidr
 
 import (
+	"math/big"
+	"net/netip"
+	"strings"
 	"testing"
 )
 
@@ -117,6 +120,90 @@ func TestExclude_Contains(t *testing.T) {
 		e := &Exclude{e: val.exclude, i: val.include}
 		if status := e.Contains(val.contains); status != val.status {
 			t.Errorf("expected value is %d, but got %d", val.status, status)
+		}
+	}
+}
+
+var testExcludeLength = []struct {
+	include CIDR
+	exclude Group
+	expect  *big.Int
+}{
+	{
+		include: must(ParsePrefix, "1.0.0.0/24"),
+		exclude: Group{arr: []Consecutive{must(ParseRange, "1.0.0.0-1.0.0.100")}},
+		expect:  big.NewInt(155),
+	},
+}
+
+func TestExclude_Length(t *testing.T) {
+	for _, val := range testExcludeLength {
+		e := &Exclude{e: val.exclude, i: val.include}
+		b := e.Length()
+		if b.Cmp(val.expect) != 0 {
+			t.Errorf("expected value is %s, but got %s", val.expect.String(), b.String())
+		}
+	}
+}
+
+var testExcludeNextIP = []struct {
+	include CIDR
+	exclude Group
+	ip      netip.Addr
+	next    netip.Addr
+}{
+	{
+		include: must(ParsePrefix, "1.0.0.0/24"),
+		exclude: Group{arr: []Consecutive{must(ParseRange, "1.0.0.3-1.0.0.100")}},
+		ip:      netip.IPv4Unspecified(),
+		next:    netip.AddrFrom4([4]byte{1, 0, 0, 0}),
+	},
+	{
+		include: must(ParsePrefix, "1.0.0.0/24"),
+		exclude: Group{arr: []Consecutive{must(ParseRange, "1.0.0.3-1.0.0.100")}},
+		ip:      netip.AddrFrom4([4]byte{1, 0, 0, 1}),
+		next:    netip.AddrFrom4([4]byte{1, 0, 0, 2}),
+	},
+	{
+		include: must(ParsePrefix, "1.0.0.0/24"),
+		exclude: Group{arr: []Consecutive{must(ParseRange, "1.0.0.3-1.0.0.100")}},
+		ip:      netip.AddrFrom4([4]byte{1, 0, 0, 3}),
+		next:    netip.AddrFrom4([4]byte{1, 0, 0, 101}),
+	},
+	{
+		include: must(ParsePrefix, "1.0.0.0/24"),
+		exclude: Group{arr: []Consecutive{must(ParseRange, "1.0.0.3-1.0.0.100")}},
+		ip:      netip.AddrFrom4([4]byte{1, 0, 0, 255}),
+		next:    netip.IPv4Unspecified(),
+	},
+}
+
+func TestExclude_NextIP(t *testing.T) {
+	for _, val := range testExcludeNextIP {
+		e := &Exclude{e: val.exclude, i: val.include}
+		if b := e.NextIP(val.ip); b.Compare(val.next) != 0 {
+			t.Errorf("expected value is %s, but got %s", val.next.String(), b.String())
+		}
+	}
+}
+
+var testExcludeString = []struct {
+	exclude Group
+	dst     []string
+}{
+	{
+		exclude: Group{arr: []Consecutive{must(ParseRange, "1.0.0.0-1.0.0.100")}},
+		dst:     []string{"1.0.0.0-1.0.0.100"},
+	},
+}
+
+func TestExclude_String(t *testing.T) {
+	for _, val := range testExcludeString {
+		e := &Exclude{e: val.exclude}
+		dst := e.Strings()
+
+		if strings.Join(dst, "|") != strings.Join(val.dst, "|") {
+			t.Errorf("expected value is %s, but got %s", val.dst, dst)
 		}
 	}
 }
