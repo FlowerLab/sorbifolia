1ed65cc29c8a621356cb543a998df1386aff3ae1
Aoang <aoang@x2oe.com>
2024-12-06 22:55:43 +0800

    feat(bunpgd): add builder OptionalUpdate

---
+++ bunpgd/builder/update_test.go
@@ -0,0 +1,46 @@
+package builder
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
+
+type testOptionalUpdate struct {
+	ID      uint64     `json:"id"`
+	SubID   uint64     `json:"sub_id"`
+	XID     *uint      `json:"-"`
+	Enable  bool       `json:"enable"`
+	Disable *bool      `json:"disable"`
+	JSON    *jsonAbleX `json:"json"`
+	User
+}
+
+type jsonAbleX struct{}
+
+func (*jsonAbleX) MarshalJSON() ([]byte, error) { return []byte("2"), nil }
+
+func TestOptionalUpdate(t *testing.T) {
+	{
+		queryBytes, err := OptionalUpdate(db.NewUpdate().Model(&User{}).Where("1=1"), &testOptionalUpdate{
+			ID:     0,
+			SubID:  0,
+			XID:    nil,
+			Enable: false,
+			JSON:   &jsonAbleX{},
+		}, "sub_id").AppendQuery(db.Formatter(), nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if string(queryBytes) != `UPDATE "user" AS "user" SET "id" = 0, "enable" = FALSE, "json" = '2' WHERE (1=1)` {
+			t.Fatal(fmt.Errorf("unexpected query: %s", string(queryBytes)))
+		}
+	}
+
+	{
+		_, err := OptionalUpdate(db.NewUpdate().Model(&User{}), map[string]any{}).AppendQuery(db.Formatter(), nil)
+		if err == nil {
+			t.Fatal(errors.New("expected error"))
+		}
+	}
+}
