851343e027f8ab3b375016a46f510dc25e4712a2
Aoang <aoang@x2oe.com>
2025-06-26 15:41:51 +0800

    feat: Implement OptionalForceUpdate for selective updates

--- bunpgd/builder/update.go
+++ bunpgd/builder/update.go
@@ -72,3 +72,77 @@ func OptionalUpdate(q *bun.UpdateQuery, v any, skip ...string) *bun.UpdateQuery 
 
 	return q
 }
+
+func OptionalForceUpdate(q *bun.UpdateQuery, v any, force, skip []string) *bun.UpdateQuery {
+	needSkip := func(key string) bool {
+		for _, s := range skip {
+			if key == s {
+				return true
+			}
+		}
+		return false
+	}
+
+	isForce := func(key string) bool {
+		for _, s := range force {
+			if key == s {
+				return true
+			}
+		}
+		return false
+	}
+
+	var (
+		rv = reflect.Indirect(reflect.ValueOf(v))
+		rt = rv.Type()
+	)
+	if rt.Kind() != reflect.Struct {
+		return q.Err(fmt.Errorf("expected a struct, got %T", v))
+	}
+
+	for i := 0; i < rt.NumField(); i++ {
+		field := rt.Field(i)
+		if field.Anonymous || !field.IsExported() {
+			continue
+		}
+
+		tag, _, _ := strings.Cut(field.Tag.Get("json"), ",")
+		if tag == "-" || needSkip(tag) {
+			continue
+		}
+
+		var (
+			kind = field.Type.Kind()
+			val  = rv.Field(i)
+		)
+
+		switch kind {
+		case reflect.Bool,
+			reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+			reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+			reflect.Float32, reflect.Float64,
+			reflect.Complex64, reflect.Complex128,
+			reflect.Array, reflect.Map, reflect.Slice, reflect.String, reflect.Struct:
+			q.Set("? = ?", bun.Ident(tag), val.Interface())
+
+		case reflect.Pointer:
+			if val.IsNil() {
+				if isForce(tag) {
+					q.Set("? = NULL", bun.Ident(tag))
+				}
+				continue
+			}
+
+			if field.Type.Elem().Kind() != reflect.Struct {
+				q.Set("? = ?", bun.Ident(tag), val.Elem().Interface())
+			} else {
+				q.Set("? = ?", bun.Ident(tag), val.Interface())
+			}
+
+		default:
+			return q.Err(fmt.Errorf("unexpected data type %s", kind))
+		}
+	}
+
+	return q
+}
--- bunpgd/builder/update_test.go
+++ bunpgd/builder/update_test.go
@@ -44,3 +44,28 @@ func TestOptionalUpdate(t *testing.T) {
 		}
 	}
 }
+
+func TestOptionalForceUpdate(t *testing.T) {
+	{
+		queryBytes, err := OptionalForceUpdate(db.NewUpdate().Model(&User{}).Where("1=1"), &testOptionalUpdate{
+			ID:     0,
+			SubID:  0,
+			XID:    nil,
+			Enable: false,
+			JSON:   &jsonAbleX{},
+		}, []string{"disable"}, []string{"sub_id"}).AppendQuery(db.Formatter(), nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if string(queryBytes) != `UPDATE "user" AS "user" SET "id" = 0, "enable" = FALSE, "disable" = NULL, "json" = '2' WHERE (1=1)` {
+			t.Fatal(fmt.Errorf("unexpected query: %s", string(queryBytes)))
+		}
+	}
+
+	{
+		_, err := OptionalForceUpdate(db.NewUpdate().Model(&User{}), map[string]any{}, nil, nil).AppendQuery(db.Formatter(), nil)
+		if err == nil {
+			t.Fatal(errors.New("expected error"))
+		}
+	}
+}
